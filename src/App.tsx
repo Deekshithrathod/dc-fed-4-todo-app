e6f40ed45835c2d3e38df04c6d44c1109df84eda
deekshith <deekshith@Mac-mini.local>
2023-08-09 19:14:39 +0530

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,20 @@ import Todos from "./components/Todos/Todos";
 
 function App() {
   return (
-    <div className="container">
-      <h1>#todo</h1>
-      <Tabs />
-      <Todos />
-    </div>
+    <>
+      <div className="container">
+        <h1>#todo</h1>
+        <Tabs />
+        <Todos />
+        <footer>
+          created by{" "}
+          <b>
+            <a href="https://github.com/Deekshithrathod">Deekshith</a>
+          </b>{" "}
+          - devChallenges.io
+        </footer>
+      </div>
+    </>
   );
 }
 
